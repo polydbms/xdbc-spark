939cbccef7dbbdc68c87d1123bf7295ca37b5b1a
harry <gavriilidis@tu-berlin.de>
2024-09-24 22:54:16 +0200

    revert old version for XDBCTable

--- src/main/scala/xdbc/XDBCTable.scala
+++ src/main/scala/xdbc/XDBCTable.scala
@@ -8,10 +8,21 @@ import org.apache.spark.sql.util.CaseInsensitiveStringMap
 import java.util
 import scala.collection.JavaConverters._
 
-class XDBCTable(schema: StructType, tableName: String) extends Table with SupportsRead {
-  override def name(): String = tableName
+class XDBCTable() extends Table with SupportsRead {
+  override def name(): String = this.getClass.toString
 
-  override def schema(): StructType = schema
+  override def schema(): StructType = StructType(Seq(
+    StructField("l_orderkey", IntegerType),
+    StructField("l_partkey", IntegerType),
+    StructField("l_suppkey", IntegerType),
+    StructField("l_linenumber", IntegerType),
+    StructField("l_quantity", DoubleType),
+    StructField("l_extendedprice", DoubleType),
+    StructField("l_discount", DoubleType),
+    StructField("l_tax", DoubleType)
+  ))
 
-  override def newScanBuilder(options: CaseInsensitiveStringMap): ScanBuilder = new SimpleScanBuilder(schema, tableName)
+  override def capabilities(): util.Set[TableCapability] = Set(TableCapability.BATCH_READ).asJava
+
+  override def newScanBuilder(options: CaseInsensitiveStringMap): ScanBuilder = new XDBCScanBuilder()
 }
